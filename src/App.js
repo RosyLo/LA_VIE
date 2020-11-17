449ea5094040fe95f406e3a737a15bdc19b1137f
RosyLo <rosyluo841107@gmail.com>
2020-11-17 10:45:07 +0800

    save package.json

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
   return (
