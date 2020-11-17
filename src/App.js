5234301b3cbe71160660a647f5c39ce0aa47428e
RosyLo <rosyluo841107@gmail.com>
2020-11-17 12:11:07 +0800

    downgrade node

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import logo from './logo.svg';
-import styles from './App.module.css';
+import styles from './App.module.scss';
 
 function App() {
   return (
