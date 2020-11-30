821db0cedd251315acb87a6d1f2d3990f83c2376
RosyLo <rosyluo841107@gmail.com>
2020-11-30 22:21:23 +0800

    add all feature

--- src/component/DeletePopup.js
+++ src/component/DeletePopup.js
@@ -2,16 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 
-// function DeletePost ({ postID }) {
-//     const dispatch = useDispatch();
-//     const user = useSelector((state) => state.user);
-
-//     return (
-
-//   }
+function DeletePost({ postID }) {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+  return (
+    <>
+      <div className='modal'>
+        <div className='leftModel'></div>
+        <div className='rightModel'>
+          <button>&times;</button>
+        </div>
+      </div>
+      <div className='overlay'></div>
+    </>
+  );
+}
 
 DeletePost.propTypes = {
   postID: PropTypes.string.isRequired,
 };
 
-// export default DeletePost;
+export default DeletePost;
--- src/component/EditPostBlock.js
+++ src/component/EditPostBlock.js
@@ -1,20 +1,25 @@
 import { React, useState } from 'react';
 import PropTypes from 'prop-types';
 import '../style/post.css';
+import DeletePopup from './DeletePopup';
 
 function EditPostBlock({ postID }) {
+  const [wantDelete, setWantDelete] = useState(false);
   return (
-    <div className='editPostBlock'>
-      <div className='editPost'>Edit</div>
-      <div
-        className='deletePost'
-        onClick={() => {
-          deletePost(postID);
-        }}>
-        Delete
+    <>
+      <div className='editPostBlock'>
+        <div className='editPost'>Edit</div>
+        <div
+          className='deletePost'
+          onClick={() => {
+            setWantDelete(!wantDelete);
+          }}>
+          Delete
+        </div>
+        <div className='checktComments'>Comments</div>
       </div>
-      <div className='checktComments'>Comments</div>
-    </div>
+      {wantDelete ? <DeletePopup postID={postID} /> : ''}
+    </>
   );
 }
 EditPostBlock.propTypes = {
